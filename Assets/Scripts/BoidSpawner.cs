d1e65ceae58dae88aa7117b932f14f3e684d23bc
Luca <lams3@cin.ufpe.br>
2021-08-26 15:16:23 -0300

    Boid Prefab

--- Assets/Scripts/BoidSpawner.cs
+++ Assets/Scripts/BoidSpawner.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class BoidSpawner : MonoBehaviour
 {
     [SerializeField]
+    private GameObject boidPrefab;
+
+    [SerializeField]
     private int numBoids;
 
     [SerializeField]
@@ -14,7 +17,7 @@ public class BoidSpawner : MonoBehaviour
         Vector3 max = this.transform.position + this.size / 2.0f;
         for (int i = 0; i < this.numBoids; i++)
         {
-            var boid = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+            var boid = Instantiate(this.boidPrefab);
             boid.transform.position = new Vector3(
                 Random.Range(min.x, max.x),
                 Random.Range(min.y, max.y),
