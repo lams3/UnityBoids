988bccef3bf22fc7b28c5dc7f700599f2ef46574
Luca <lams3@cin.ufpe.br>
2021-08-27 11:16:12 -0300

    Boid Perception Radius

--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Boid : MonoBehaviour
@@ -8,12 +9,20 @@ public class Boid : MonoBehaviour
     [Range(0, 10)]
     public float maxSpeed = 4.0f;
 
+    [Range(0, 10)]
+    public float perceptionRadius = 2.0f;
+
     [HideInInspector]
     public Vector3 velocity;
-
 
     private void Update()
     {
         this.transform.position += this.velocity * Time.deltaTime;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1.0f, 1.0f, 0.0f, 0.2f);
+        Gizmos.DrawSphere(this.transform.position, this.perceptionRadius);
     }
 }
